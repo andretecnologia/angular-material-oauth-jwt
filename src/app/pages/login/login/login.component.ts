14cb364c66054141e8837bf32339c9a0d558c7df
rafaelnilton <rafaelniltonwd@hotmail.com>
2020-07-14 18:45:20 -0300

    eye on password field

--- src/app/pages/login/login/login.component.ts
+++ src/app/pages/login/login/login.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
-
+  hide = true;
   constructor(private authService : AuthService, private router: Router) { }
 
 
